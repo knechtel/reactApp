a6b28518187dd16a4175f7920e48d9a6dc1b8e59
Maiquel Knechtel <maiquelknechtel@MacBook-Pro-de-Maiquel.local>
2018-07-03 21:42:48 -0300

    first commit

---
+++ cdc-admin/src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import {FormularioAutor,TabelaAutores} from './Autor';
+import AutorBox from './Autor';
+class App extends Component {
+
+  constructor() {
+    super();    
+    this.state = {lista : [],nome:'',email:'',senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  componentDidMount(){  
+    $.ajax({
+        url:"http://localhost:8080/api/autores",
+        dataType: 'json',
+        success:function(resposta){    
+          this.setState({lista:resposta});
+        }.bind(this)
+      } 
+    );          
+  }
+
+  enviaForm(evento){
+    evento.preventDefault();    
+    $.ajax({
+      url:'http://localhost:8080/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(resposta){
+        this.setState({lista:resposta});        
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }      
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }  
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }
+  render() {    
+    return (
+      <div id="layout">
+          
+          <a href="#menu" id="menuLink" className="menu-link">
+              
+              <span></span>
+          </a>
+
+          <div id="menu">
+              <div className="pure-menu">
+                  <a className="pure-menu-heading" href="#">Company</a>
+
+                  <ul className="pure-menu-list">
+                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+
+                      
+                  </ul>
+              </div>
+        </div>
+
+
+      <div id="main">
+        <div className="header">
+          <h1>Cadastro de Autores</h1>
+        </div>
+        <div className="content" id="content">
+          <AutorBox/>  
+        </div>
+      </div>
+    </div>
+
+
+    
+                          
+
+   
+    );
+  }
+}
+
+export default App;
---
+++ cdc-admin/src/TratadorErros.js
@@ -0,0 +1,7 @@
+export default class TratadorErros{
+
+    publicaErros(erros){
+
+	}
+
+}
